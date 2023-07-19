d7699ac0b3deeeb2d83e5468e053d2365ec8f3b5
paulaxisabel <picsigno@gmail.com>
2023-07-19 15:24:19 +0800

    update functionality to automatically generate text in real-time

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 // Generate text
-const generateButton = document.querySelector(".generate-button");
 const resetButton = document.querySelector(".reset-button");
 const copyButton = document.querySelector(".copy-button");
 const textInput = document.getElementById("textInput");
 const generatedText = document.getElementById("generatedText");
 const copyAlert = document.getElementById("copyAlert");
 const emptyAlert = document.getElementById("emptyAlert");
-generateButton.addEventListener("click", () => {
+
+textInput.addEventListener("input", () => {
   const text = textInput.value.trim();
   if (text === "") {
-    showEmptyAlert();
+    generatedText.value = "";
+    resetButton.style.display = "none";
   } else {
     const generated = text.replace(/ /g, " 🤸 ");
     generatedText.value = generated;
     resetButton.style.display = "block";
   }
 });
+
 copyButton.addEventListener("click", () => {
   const generated = generatedText.value.trim();
   if (generated === "") {
@@ -34,6 +36,7 @@ copyButton.addEventListener("click", () => {
     }, 3000);
   }
 });
+
 resetButton.addEventListener("click", () => {
   textInput.value = "";
   generatedText.value = "";
@@ -41,15 +44,19 @@ resetButton.addEventListener("click", () => {
 });
 
 function showEmptyAlert() {
+  var emptyAlert = document.getElementById("emptyAlert");
+  emptyAlert.style.opacity = "1";
   emptyAlert.style.display = "block";
+
   setTimeout(() => {
-    emptyAlert.style.display = "none";
-  }, 3000);
+    emptyAlert.style.opacity = "0";
+    setTimeout(() => {
+      emptyAlert.style.display = "none";
+    }, 1000);
+  }, 5000);
 }
-generatedText.addEventListener("input", () => {
-  const generated = generatedText.value.trim();
-  resetButton.style.display = generated !== "" ? "block" : "none";
-});
+
+
 // Dark mode toggle
 const darkModeSwitch = document.getElementById("darkModeSwitch");
 const darkModeSwitchInner = document.getElementById("darkModeSwitchInner");
@@ -62,11 +69,19 @@ darkModeSwitch.addEventListener("change", () => {
     body.style.color = "#f1f5f9";
     footer.style.backgroundColor = "#27272a";
     footer.style.color = "#f1f5f9";
+    textInput.style.backgroundColor = "#353537";
+    textInput.style.color = "#f1f5f9";
+    generatedText.style.backgroundColor = "#353537";
+    generatedText.style.color = "#f1f5f9";
   } else {
     body.style.backgroundColor = "#f1f5f9";
     body.style.color = "#27272a";
     footer.style.backgroundColor = "#f1f5f9";
     footer.style.color = "#27272a";
+    textInput.style.backgroundColor = "#f8f9fa";
+    textInput.style.color = "#000000";
+    generatedText.style.backgroundColor = "#f8f9fa";
+    generatedText.style.color = "#000000";
   }
 });
 
